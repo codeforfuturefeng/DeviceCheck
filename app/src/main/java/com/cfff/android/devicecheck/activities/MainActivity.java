412777ea59c86269f151dd07269c9f2afc77ee79
codeforfuturefeng <code.for.future.feng@gmail.com>
2017-12-31 23:17:56 +0800

    remove un-use things

---
+++ app/src/main/java/com/cfff/android/devicecheck/activities/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cfff.android.devicecheck.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
