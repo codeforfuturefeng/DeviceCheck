1a41606c4b8f0c4929704b1d00862fd0dd593715
codeforfuturefeng <code.for.future.feng@gmail.com>
2017-12-31 23:17:56 +0800

    remove un-use things

---
+++ app/src/main/java/com/cfff/android/devicecheck/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.cfff.android.devicecheck.activities;
+
+import android.app.Activity;
+
+/**
+ * Created by guangweifeng on 31/12/2017.
+ */
+
+public class BaseActivity extends Activity {
+}
